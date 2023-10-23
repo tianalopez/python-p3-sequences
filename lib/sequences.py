cfa3feee65ea60a4eb5e285aae59e0cb3481174e
Tiana Lopez <tianadavin@gmail.com>
2023-10-23 10:57:55 -0700

    passes tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    new_sequence = []
+    if length > 0:
+        new_sequence.append(0)
+    if length > 1 :
+        new_sequence.append(1)
+    if length > 2:
+        i=0
+        while i <= (length - 3):
+            new_value = new_sequence[i] + new_sequence[i+1]
+            new_sequence.append(new_value)
+            i+=1
+    print(new_sequence)
